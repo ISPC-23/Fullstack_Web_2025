42e4babbfdc2ed14891d72905a6b0449bf53923f
Fede Krenn <fedekrenn@gmail.com>
2024-06-17 19:36:26 -0300

    Refactor sobre componente 'about-us'

--- frontend/src/app/components/about-us/about-us.component.ts
+++ frontend/src/app/components/about-us/about-us.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Member {
+  name: string;
+  img: string;
+  link: string;
+}
+
 @Component({
   selector: 'app-about-us',
   standalone: true,
@@ -7,4 +13,14 @@ import { Component } from '@angular/core';
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css',
 })
-export class AboutUsComponent {}
+export class AboutUsComponent {
+  members: Member[] = [
+    { name: 'Barletta Fernando', img: 'fernando.jpeg', link: 'Ferbarletta' },
+    { name: 'Blasiche Andres', img: 'andres.jpeg', link: 'blasichea' },
+    { name: 'Cabrera Veronica', img: 'veronica.jpeg', link: 'Verosolc30' },
+    { name: 'Castillo Fernanda', img: 'fernanda.png', link: 'FernandaACastillo' },
+    { name: 'Krenn Federico', img: 'federico.jpeg', link: 'fedekrenn' },
+    { name: 'Liendo Germán', img: 'german.jpg', link: 'g3rm6n' },
+    { name: 'Gillini Emiliano', img: 'emiliano.jpg', link: 'emigillini' },
+  ];
+}
